721aa0119d71249e10a5670c949639386d2ace0d
mygoaway <mygoaway2@gmail.com>
2022-07-03 23:04:15 +0900

    section 1 commit

---
+++ src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,7 @@
+package hello.jdbc.connection;
+
+public abstract class ConnectionConst {
+    public static final String URL = "jdbc:mysql://localhost:3306/mysql?useSSL=false&serverTimezone=UTC&characterEncoding=UTF-8&allowPublicKeyRetrieval=true";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "akrhkd88@@";
+}
